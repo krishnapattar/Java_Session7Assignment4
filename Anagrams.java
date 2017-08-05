f32b46a2f90628657c769a9d54e5b8214fc7d9cd
krishnapattar <krishnapattar140@gmail.com>
2017-08-05 16:47:48 +0530

    Add files via upload

---
+++ Anagrams.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+/*
+ * Write a program that finds all anagram sets from a list of words.
+ */
+public class Anagrams {//proper naming conventions is used 
+
+	int c = 0;
+    
+    void input()throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word : ");
+        String s = sc.next();
+        System.out.println("The Anagrams are : ");
+        display("",s);
+        System.out.println("Total Number of Anagrams = "+c);
+    }
+   
+    void display(String s1, String s2)
+    {
+        if(s2.length()<=1)
+        {
+            c++;
+            System.out.println(s1+s2);
+        }
+        else
+        {
+            for(int i=0; i<s2.length(); i++)
+            {
+                String x = s2.substring(i, i+1);
+                String y = s2.substring(0, i);
+                String z = s2.substring(i+1);
+                display(s1+x, y+z);
+            }
+        }
+    }
+     
+    public static void main(String args[])throws Exception// Main methods starts
+    {
+        Anagrams ob=new Anagrams();
+        ob.input();
+    }
+}// class closed
